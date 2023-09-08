26150cd77b17a065431b973f4216e6c60ce5046d
Roy Uri <94353032+royuri@users.noreply.github.com>
2023-09-08 12:36:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-  const APIKey = '6a59479195f7ddbe44436c2ddc32bf86';
+  const APIKey = '';
   const city = document.querySelector('.search-box input').value;
 
   if (city === '')
